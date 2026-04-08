dfdd01fd4263d8b157c4a0a7aa1d246a639ac6c9
sageye15 <es-asmaa.ghanem2026@alexu.edu.eg>
2026-04-09 00:15:12 +0200

    working need final test

--- movie-catalog-service/src/main/java/com/moviecatalogservice/resources/TrendingResource.java
+++ movie-catalog-service/src/main/java/com/moviecatalogservice/resources/TrendingResource.java
@@ -8,7 +8,11 @@ import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
@@ -18,12 +22,21 @@ public class TrendingResource {
     private TrendingServiceGrpc.TrendingServiceBlockingStub trendingStub;
 
     @GetMapping("/trending")
-    public List<TopRatedMovie> getTopTrendingMovies() {
+    public List<Map<String, Object>> getTopTrendingMovies() {
         TrendingMoviesRequest request = TrendingMoviesRequest.newBuilder()
                 .setTopN(10)
                 .build();
 
-       TrendingMoviesResponse response = trendingStub.getTopRatedMovies(request);
-        return response.getMoviesList();
+        TrendingMoviesResponse response = trendingStub.getTopRatedMovies(request);
+
+        return response.getMoviesList().stream()
+                .map(movie -> {
+                    Map<String, Object> m = new HashMap<>();
+                    m.put("movieId", movie.getMovieId());
+                    m.put("movieName", movie.getMovieName());
+                    m.put("rating", movie.getRating());
+                    return m;
+                })
+                .collect(Collectors.toList());
     }
 }
